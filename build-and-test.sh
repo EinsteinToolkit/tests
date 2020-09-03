0ac7aa9e0e1555ab1b9a006d361a9284b8d7a608
Mohammed Jamil <mjamil90@stuy.edu>
2020-09-03 12:48:15 -0400

    added build and test shell script and python html generation script

---
+++ build-and-test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export SYNC_SUBMODULES=true
+export CLEAN_CACTUS_JENKINS=true
+export BUILD_TYPE=Incremental
+cd einsteintoolkit
+export WORKSPACE=$PWD
+set -e -x
+
+export ENABLED_THORNS="
+  CactusElliptic/EllPETSc
+  CactusUtils/TATPETSc
+  ExternalLibraries/PETSc
+"
+
+rm -f build__*.log
+# Work around bugs in Jenkins
+
+if [ "$SYNC_SUBMODULES" = "true" ]; then
+  git submodule sync
+fi
+
+git submodule update --init #--force
+# undo any local changes (do not use --force above since it always touches
+# files)
+git submodule foreach "git diff --quiet || git reset --hard"
+
+if [ "$CLEAN_CACTUS_JENKINS" = "true" -o ! -r $WORKSPACE/cactusjenkins ]; then
+  rm -rf $WORKSPACE/cactusjenkins
+  git clone https://bitbucket.org/ianhinder/cactusjenkins.git $WORKSPACE/cactusjenkins
+fi
+if [ -r $WORKSPACE/configs/sim ]; then
+  ( cd $WORKSPACE; make sim-cleandeps )
+fi
+time $WORKSPACE/cactusjenkins/build-cactus manifest/einsteintoolkit.th
+time $WORKSPACE/cactusjenkins/test-cactus all
+python3 $WORKSPACE/logpage.py
+# it takes ~1hr to build the docs
+#time $WORKSPACE/cactusjenkins/build-cactus-doc
+
+#cd $WORKSPACE/repos/carpet
+#doxygen
---
+++ logpage.py
@@ -0,0 +1,55 @@
+import sys
+def create_summary(file):
+    sum_data={}
+    with open(file,"r") as fp:
+        lines=fp.read().splitlines()
+        sum_start=lines.index("  Summary for configuration sim")+6
+        i=sum_start
+        while lines[i]!="  Tests passed:":
+            if lines[i]=="    Number passed only to" and lines[i]!="":
+                split_l=lines[i+1].split("->")
+                split_l[0]=lines[i]+" "+split_l[0]
+                try:
+                    sum_data[" ".join(split_l[0].split())]=int(split_l[1].strip())
+                except:
+                    sum_data[" ".join(split_l[0].split())]=split_l[1]
+                i+=1
+            elif lines[i]!="":
+                split_l=lines[i].split("->")
+                try:
+                    sum_data[" ".join(split_l[0].split())]=int(split_l[1].strip())
+                except:
+                    sum_data[" ".join(split_l[0].split())]=split_l[1]
+            i+=1
+            
+    return sum_data
+    
+
+
+def summary_to_html(readfile,writefile):
+    data=create_summary(readfile)
+    contents=""
+    status="All Tests Passed"
+    if data["Number of tests passed"]==0:
+        status="All Tests Passed"
+    elif data["Number failed"]!=0:
+        status="Some Tests Failed"
+    with open(writefile,"w") as fp:
+        for key in data.keys():
+            contents+=f"        <tr><td>{key}</td><td>{data[key]}</td><tr>\n"
+        template=f'''<!doctype html>
+    <html lang="en">
+        <head>
+            <title>Summary of Tests</title>
+        </head>
+        <body>
+            <h1 style="text-align:center">{status}</h1>
+            <table style="border: 1px solid black;margin-left: auto;margin-right: auto;">
+            {contents}
+            </table>
+            
+        </body>
+    </html>
+        '''
+        fp.write(template)
+summary_to_html("build__2_2.log","index.html")
