f7262d983c0e1e325844c59fb524c75ae1db29b7
Mohammed S. Jamil <msj2340@gmail.com>
2020-09-17 17:40:19 -0400

    Update build-and-test.sh

--- build-and-test.sh
+++ build-and-test.sh
@@ -36,7 +36,6 @@ sed -i '2a export WORKSPACE=$PWD ' cactusjenkins/test-cactus
 sed -i '2a export JOB_NAME="TestJob00" ' cactusjenkins/test-cactus
 time $WORKSPACE/cactusjenkins/test-cactus all
 python3 $WORKSPACE/logpage.py
-python3 $WORKSPACE/mail.py
 
 # it takes ~1hr to build the docs
 #time $WORKSPACE/cactusjenkins/build-cactus-doc
