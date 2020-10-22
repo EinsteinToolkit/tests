095e0d64f6923e2e358ef1dd5f5533a7da857673
Mohammed Jamil <mjamil90@stuy.edu>
2020-10-22 11:09:13 -0400

    improved html page

--- logpage.py
+++ logpage.py
@@ -1,3 +1,4 @@
+from requests.api import get
 from cactusjenkins.testtable.testtable import read_file
 import sys
 import os,csv,time,requests
@@ -8,6 +9,10 @@ import bokeh.models.tools as btools
 import bokeh.models.callbacks as bcall
 from bokeh.resources import CDN
 from bokeh.embed import components
+from bokeh.layouts import row
+from store import get_version
+from parser import create_summary,get_tests,test_comp,get_times,exceed_thresh,longest_tests,get_unrunnable,get_data
+import glob
 # This part finds the second log file in the folder
 log=""
 logs=[]
@@ -16,96 +21,11 @@ for fp in os.listdir("./"):
         log=os.path.join("./", fp)
         logs.append(log)
 records=os.listdir("./records")
-last=records[len(records)-1]
-ext=last.find(".log")
-num=int(last[ext-1:ext])
+last_ver=get_version()-1
+last=f"build__2_1_{last_ver}.log"
 
 
 
-
-
-def create_summary(file):
-    '''
-        This function parses the test results from the build__2_1.log file
-        into a dictionary
-    '''
-    sum_data={}
-    with open(file,"r") as fp:
-        lines=fp.read().splitlines()
-        # Find the line where the summary starts
-        sum_start=lines.index("  Summary for configuration sim")+6
-        i=sum_start
-        # Loop until the end of the summary
-        while lines[i]!="  Tests passed:":
-            # The spacing of this line is unique and as such requires a special if statement
-            if lines[i]=="    Number passed only to" and lines[i]!="":
-                split_l=lines[i+1].split("->")
-                split_l[0]=lines[i]+" "+split_l[0]
-
-                # Convert numerical data to integer
-                try:
-                    sum_data[" ".join(split_l[0].split())]=int(split_l[1].strip())
-                except:
-                    sum_data[" ".join(split_l[0].split())]=split_l[1]
-                # This data field has to lines and as such increment the line counter twice
-                i+=1
-
-            # The regular line parsing code
-            elif lines[i]!="":
-                split_l=lines[i].split("->")
-                try:
-                    sum_data[" ".join(split_l[0].split())]=int(split_l[1].strip())
-                except:
-                    sum_data[" ".join(split_l[0].split())]=split_l[1]
-            i+=1
-            
-    return sum_data
-    
-
-
-
-def get_tests(readfile):
-    '''
-        This functions reads the names of the tests that failed and passed
-        from the log files and returns the set of them in tuples.
-    '''
-    passed=set()
-    failed=set()
-    with open(readfile,"r") as fp:
-        lines=fp.read().splitlines()
-        ind=lines.index("  Tests passed:")+2
-        while lines[ind]!="  Tests failed:" and lines[ind]!="========================================================================":
-            passed.add(" ".join(lines[ind].split()))
-            ind+=1
-        ind+=2
-        while ind<len(lines) and lines[ind]!="========================================================================":
-            failed.add(" ".join(lines[ind].split()))
-            ind+=1
-        if "" in passed:
-            passed.remove("")
-        if "" in failed:
-            failed.remove("")
-    return passed,failed
-
-
-
-def test_comp(readfile_new,readfile_old):
-    '''
-        This function compares the tests from the previous run 
-        to see which failed, which failed this time but did
-        not in the last, which passed this time but not in the last,
-        and the new and removed tests.
-    '''
-    passed_n,failed_n=get_tests(readfile_new)
-    passed_o,failed_o=get_tests(readfile_old)
-    newly_p=passed_n-passed_o
-    newly_f=failed_n-failed_o
-    new_tests=(passed_n.union(failed_n))-(passed_o.union(failed_o))
-    missing_tests=(passed_o.union(failed_o))-(passed_n.union(failed_n))
-    types=["Failed Tests","Newly Passing Tests","Newly Failing Tests","Newly Added Tests", "Removed Tests"]
-    tests=[failed_n,newly_p,newly_f,new_tests,missing_tests]
-    test_dict={types[i]:tests[i] for i in range(len(types))}
-    return test_dict
 
 commit_list=requests.get("https://api.github.com/repos/mojamil/einsteintoolkit/commits")
 response=commit_list.json()
@@ -114,7 +34,7 @@ previous=response[1]["sha"]
 compare=requests.get(f"https://api.github.com/repos/mojamil/einsteintoolkit/compare/{previous}...{current}")
 diff=compare.json()["diff_url"]
 
-log_link=f"https://github.com/mojamil/einsteintoolkit/blob/master/records/{last[ext-1:]+str(num+1)}"
+# log_link=f"https://github.com/mojamil/einsteintoolkit/blob/master/records/{last[ext-1:]+str(last_ver+1)}"
 def gen_report(readfile):
     '''
         This function generates the html table that shows
@@ -122,97 +42,69 @@ def gen_report(readfile):
         by test_comp
     '''
     test_comparison=test_comp(readfile,"./records/"+last)
-    output='''<table style="border: 1px solid black;margin-left: auto;margin-right: auto;">\n'''
+    output='''<table style="border: 1px solid black;margin-left: auto;margin-right: auto;">
+    <caption style="text-align:center;font-weight: bold;caption-side:top">Failed Tests and Changes</caption>\n'''
+
     for result in test_comparison.keys():
         if(result!="Failed Tests"):
             output+=f"<tr><th><a href='{diff}'>"+result+"</a></th></tr>\n"
         else:
-            output+=f"<tr><th><a href='{log_link}'>"+result+"</a></th></tr>\n"
+            output+=f"<tr><th>"+result+"</th>"
+            output+="<th>logs</th><th>diffs</th>"
+        
         if(len(test_comparison[result])==0):
             output+="<tr><td></td></tr>"
 
         for test in test_comparison[result]:
-            output+=f"  <tr><td>{test}</td></tr>\n"
+            if("Removed" not in result):
+                thorn=test.split()[-1]
+                thorn=thorn[:len(thorn)-1]
+                test_name=test.split()[0]
+                logl=f"https://github.com/mojamil/einsteintoolkit/tree/gh-pages/records/sim_{last_ver}/{thorn}/{test_name}.log"
+                diffl=f"https://github.com/mojamil/einsteintoolkit/tree/gh-pages/records/sim_{last_ver}/{thorn}/{test_name}.diffs"
+                output+=f"  <tr><td>{test}</td><td><a href='{logl}'>log</a></td><td><a href='{diffl}'>diff</a></td></tr>\n"
+            else:
+                thorn=test.split()[-1]
+                thorn=thorn[:len(thorn)-1]
+                test_name=test.split()[0]
+                logl=f"https://github.com/mojamil/einsteintoolkit/tree/gh-pages/records/sim_{last_ver-1}/{thorn}/{test_name}.log"
+                diffl=f"https://github.com/mojamil/einsteintoolkit/tree/gh-pages/records/sim_{last_ver-1}/{thorn}/{test_name}.diffs"
+                output+=f"  <tr><td>{test}</td><td><a href='{logl}'>log</a></td><td><a href='{diffl}'>diff</a></td></tr>\n"
     
     output+="</table>"
     return output
 
-def get_times(readfile):
-    '''
-        This function finds the times taken for each test in the log
-        file and then stores that in a dictionary and then sorts
-        those tests in descending order by time
-    '''
-    times={}
-    with open(readfile,"r") as fp:
-        lines=fp.read().splitlines()
-        ind=lines.index("  Details:")+2
-        while lines[ind] !="  Thorns with no valid testsuite parameter files:":
-            try:
-                time_i=lines[ind].index('(')
-                tim=float(lines[ind][time_i+1:].split()[0])
-                test_name=lines[ind][:time_i-1].split()[0]
-                times[test_name]=tim
-            except:
-                pass
-            ind+=1
-
-    return {test:ti for test,ti in sorted(times.items(),key= lambda x : x[1],reverse=True)} # This is a dictionary comprehension that uses sorted to order the items in times.items() into a dictionary
-
-def exceed_thresh(time_dict,thresh):
-    '''
-        This function finds tests that exceed a certain time threshhold
-    '''
-    return {test:ti for test,ti in time_dict.items() if ti>thresh}
-
-
-def longest_tests(time_dict,num_tests):
-    '''
-        This function finds the tests that took the longest time.
-        It returns a dictionary of the num_test number of longest
-        tests.
-    '''
-    longest={}
-    i=0
-    for item in time_dict.items():
-        if i>num_tests-1:
-            break
-        longest[item[0]]=item[1]
-        i+=1
-    return longest
 
 def gen_time(readfile):
     time_dict=get_times(readfile)
-    output=''' <table style="border: 1px solid black;margin-left: auto;margin-right: auto;">\n'''
-    output+="<tr><th>Longest Tests</th></tr>\n"
+    output='''<table style="border: 1px solid black;margin-left: auto;margin-right: auto;">
+    <caption style="text-align:center;font-weight: bold;caption-side:top">Longest Tests</caption>\n'''
+    output+="<tr><th>Test Name</th><th>Running Time</th>"
     for times in longest_tests(time_dict,10).keys():
-        output+=f"  <tr><td>{times}</td><td>{time_dict[times]}s</td></tr>\n"
+        output+=f"   <tr><td>{times}</td><td>{time_dict[times]}s</td></tr>\n"
     output+="</table><br>"
-    output+='''<table style="border: 1px solid black;margin-left: auto;margin-right: auto;">\n'''
-    output+="<tr><th>Tests That Take More Than 20s</th></tr>\n"
-    for times in exceed_thresh(time_dict,20).keys():
-        output+=f"  <tr><td>{times}</td><td>{time_dict[times]}s</td></tr>\n"
-    output+="</table>"
     return output
 
 def plot_test_data():
     dat=list(get_data("Runnable tests").values())
     times=list(get_data("Number of tests passed").keys())
     dat1=list(get_data("Number of tests passed").values())
+    dat2=list(get_data("Time Taken").values())
     print(dat)
     src=bplt.ColumnDataSource(data=dict(
         t=times,
         rt=dat,
         tp=dat1,
+        timet=dat2,
         xax=[0]*len(times),
-        url=[""]*len(times),
+        url=[f"./index_{x+1}.html" for x in range(last_ver)]+["index.html"],
     ))
     TOOLTIPS = [
         ("Tests Passed", "$tp"),
     ]
     print(src.data["rt"])
     p=bplt.figure(x_range=times,plot_width=600, plot_height=600,tools="tap",
-           title=None, toolbar_location="below")
+           title="Passed Tests", toolbar_location="below")
     p.circle(times,dat,size=10,color="green")
     p.circle('t','tp',size=10,color="blue",source=src)
     url = "@url"
@@ -220,10 +112,30 @@ def plot_test_data():
     taptool.callback = bcall.OpenURL(url=url)
     p.varea(y1='rt',y2='xax', x='t', color="green",source=src,alpha=0.5)
     p.varea(y1='tp',y2='xax', x='t', color="blue",source=src,alpha=0.5)
+    p1=bplt.figure(x_range=times,plot_width=600, plot_height=600,tools="tap,wheel_zoom,box_zoom,reset",
+           title="Time Taken", toolbar_location="below")
+    p1.circle('t','timet',size=10,color="blue",source=src)
+    taptool = p1.select(type=btools.TapTool)
+    taptool.callback = bcall.OpenURL(url=url)
     
-    script, div = components(p)
+    script, div = components(row(p,p1))
     
     return script,div
+
+
+def gen_unrunnable(readfile):
+    m,n=get_unrunnable(readfile)
+    output=''' <table style="border: 1px solid black;margin-left: auto;margin-right: auto;">
+    <caption style="text-align:center;font-weight: bold;caption-side:top">Unrunnable Tests</caption>\n'''
+    output+="<tr><th>Tests Missed for Lack Of Thorns</th><th>Missing Thorns</th></tr>\n"
+    for test in m.keys():
+        thorns=','.join(m[test])
+        output+=f"  <tr><td>{test}</td><td>{thorns}</td></tr>\n"
+    output+="<tr><th>Tests missed for different number of processors required:</th><th>Processors Required</th></tr>\n"
+    for test in n.keys():
+        output+=f"  <tr><td>{test}</td><td>{n[test]}</td></tr>\n"
+    output+="</table>"
+    return output
 
 def summary_to_html(readfile,writefile):
     '''
@@ -244,6 +156,7 @@ def summary_to_html(readfile,writefile):
         status="Some Tests Failed"
         # Send email if tests failed
         #os.system(f'python3 mail.py')
+    sidebar=gen_sidebar()
     with open(writefile,"w") as fp:
         for key in data.keys():
 
@@ -255,9 +168,35 @@ def summary_to_html(readfile,writefile):
     <html lang="en">
         <head>
             <title>Summary of Tests</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <style>
             .bk-root .bk {{
                 margin: 0 auto !important;
+            }}
+            </style>
+            <style>
+            .sidebar {{
+                height: 100%; 
+                width: 150px;
+                position: fixed;
+                z-index: 1; 
+                top: 0; 
+                left: 0;
+                background-color: #111; 
+                overflow-x: hidden;
+                padding-top: 20px;
+            }}
+
+            .sidebar a {{
+                padding: 6px 8px 6px 16px;
+                text-decoration: none;
+                font-size: 18px;
+                color: lightgray;
+                display: block;
+                }}
+
+            .sidebar a:hover {{
+                color: #f1f1f1;
             }}
             </style>
             <script src="https://cdn.bokeh.org/bokeh/release/bokeh-2.0.1.min.js"
@@ -265,19 +204,27 @@ def summary_to_html(readfile,writefile):
             {script}
         </head>
         <body>
-            <h1 style="text-align:center">{status}</h1>
-            <table style="border: 1px solid black;margin-left: auto;margin-right: auto;">
-            {contents}
-            </table>
-            <br>
-            {gen_report(readfile)}
-            <br>
-            {gen_time(readfile)}
-            <br>
-            <table style="margin: 0 auto;">
-                {div}
-            </table>
-            
+            <div class="sidebar">
+            {sidebar}
+            </div>
+            <div class="container">
+                <h1 style="text-align:center">{status}</h1>
+                <h3 style="text-align:center">Build #{last_ver+1}</h3>
+                <table style="border: 1px solid black;margin-left: auto;margin-right: auto;">
+                <caption style="text-align:center;font-weight: bold;caption-side:top">Summary</caption>
+                {contents}
+                </table>
+                <br>
+                {gen_report(readfile)}
+                <br>
+                {gen_time(readfile)}
+                <br>
+                {gen_unrunnable(readfile)}
+                <br>
+                <table style="margin: 0 auto;">
+                    {div}
+                </table>
+            <div>
             
         </body>
     </html>
@@ -303,25 +250,18 @@ def write_to_csv(readfile):
 
 #write_to_csv(log)
 
-def plot_data(name,data,dates):
-    plt.xlabel("Date of Test")
-    plt.ylabel(name)
-    plt.plot(dates,data,'bo-')
-    plt.fill_between(dates,0,data,facecolor='blue',alpha=0.3)
-    plt.savefig("./docs/plot.png")
+def gen_sidebar():
+    sidebar=""
+    for i in range(1,last_ver+1):
+        sidebar+=f'''   <a href="index_{i}.html">Build #{i}</a>\n'''
+    sidebar+=f'''   <a href="index.html">Build #{i+1}</a>\n'''
+    return sidebar
+        
 
 
-def get_data(name):
-    data={}
-    with open('test_nums.csv','r') as csvfile:
-        fields=csvfile.readline().strip().split(",")
-        name_i=fields.index(name)
-        line=csvfile.readline()
-        while line !="":
-            entry=line.strip().split(",")
-            data[entry[0]]=float(entry[name_i])
-            line=csvfile.readline()
-    return data
+
+
+
 
 
 
@@ -330,7 +270,6 @@ def get_data(name):
 
 summary_to_html(log,"docs/index.html")
 
-command='''for file in *log;do cp "$file" "./records/${file%.log}_'''+str(num+1)+'''.log";done'''
 #os.system(command)
 
 
---
+++ parser.py
@@ -0,0 +1,146 @@
+def create_summary(file):
+    '''
+        This function parses the test results from the build__2_1.log file
+        into a dictionary
+    '''
+    sum_data={}
+    with open(file,"r") as fp:
+        lines=fp.read().splitlines()
+        # Find the line where the summary starts
+        sum_start=lines.index("  Summary for configuration sim")+6
+        i=sum_start
+        # Loop until the end of the summary
+        while lines[i]!="  Tests passed:":
+            # The spacing of this line is unique and as such requires a special if statement
+            if lines[i]=="    Number passed only to" and lines[i]!="":
+                split_l=lines[i+1].split("->")
+                split_l[0]=lines[i]+" "+split_l[0]
+
+                # Convert numerical data to integer
+                try:
+                    sum_data[" ".join(split_l[0].split())]=int(split_l[1].strip())
+                except:
+                    sum_data[" ".join(split_l[0].split())]=split_l[1]
+                # This data field has to lines and as such increment the line counter twice
+                i+=1
+
+            # The regular line parsing code
+            elif lines[i]!="":
+                split_l=lines[i].split("->")
+                try:
+                    sum_data[" ".join(split_l[0].split())]=int(split_l[1].strip())
+                except:
+                    sum_data[" ".join(split_l[0].split())]=split_l[1]
+            i+=1
+            
+    return sum_data
+
+def get_tests(readfile):
+    '''
+        This functions reads the names of the tests that failed and passed
+        from the log files and returns the set of them in tuples.
+    '''
+    passed=set()
+    failed=set()
+    with open(readfile,"r") as fp:
+        lines=fp.read().splitlines()
+        ind=lines.index("  Tests passed:")+2
+        while lines[ind]!="  Tests failed:" and lines[ind]!="========================================================================":
+            passed.add(" ".join(lines[ind].split()))
+            ind+=1
+        ind+=2
+        while ind<len(lines) and lines[ind]!="========================================================================":
+            failed.add(" ".join(lines[ind].split()))
+            ind+=1
+        if "" in passed:
+            passed.remove("")
+        if "" in failed:
+            failed.remove("")
+    return passed,failed
+
+def test_comp(readfile_new,readfile_old):
+    '''
+        This function compares the tests from the previous run 
+        to see which failed, which failed this time but did
+        not in the last, which passed this time but not in the last,
+        and the new and removed tests.
+    '''
+    passed_n,failed_n=get_tests(readfile_new)
+    passed_o,failed_o=get_tests(readfile_old)
+    newly_p=passed_n-passed_o
+    newly_f=failed_n-failed_o
+    new_tests=(passed_n.union(failed_n))-(passed_o.union(failed_o))
+    missing_tests=(passed_o.union(failed_o))-(passed_n.union(failed_n))
+    types=["Failed Tests","Newly Passing Tests","Newly Failing Tests","Newly Added Tests", "Removed Tests"]
+    tests=[failed_n,newly_p,newly_f,new_tests,missing_tests]
+    test_dict={types[i]:tests[i] for i in range(len(types))}
+    return test_dict
+
+def get_times(readfile):
+    '''
+        This function finds the times taken for each test in the log
+        file and then stores that in a dictionary and then sorts
+        those tests in descending order by time
+    '''
+    times={}
+    with open(readfile,"r") as fp:
+        lines=fp.read().splitlines()
+        ind=lines.index("  Details:")+2
+        while lines[ind] !="  Thorns with no valid testsuite parameter files:":
+            try:
+                time_i=lines[ind].index('(')
+                tim=float(lines[ind][time_i+1:].split()[0])
+                test_name=lines[ind][:time_i-1].split()[0]
+                times[test_name]=tim
+            except:
+                pass
+            ind+=1
+
+    return {test:ti for test,ti in sorted(times.items(),key= lambda x : x[1],reverse=True)} # This is a dictionary comprehension that uses sorted to order the items in times.items() into a dictionary
+def exceed_thresh(time_dict,thresh):
+    '''
+        This function finds tests that exceed a certain time threshhold
+    '''
+    return {test:ti for test,ti in time_dict.items() if ti>thresh}
+
+def longest_tests(time_dict,num_tests):
+    '''
+        This function finds the tests that took the longest time.
+        It returns a dictionary of the num_test number of longest
+        tests.
+    '''
+    longest={}
+    i=0
+    for item in time_dict.items():
+        if i>num_tests-1:
+            break
+        longest[item[0]]=item[1]
+        i+=1
+    return longest
+
+def get_unrunnable(readfile):
+    miss_th={}
+    miss_proc={}
+    with open(readfile,"r") as fp:
+        lines=fp.read().splitlines()
+        ind=lines.index("  Tests missed for lack of thorns:")+2
+        while lines[ind] !="  Tests missed for different number of processors required:":
+            missing=lines[ind+2].split(":")[1].split()
+            miss_th[lines[ind].strip()]=missing
+            ind+=4
+        ind+=2
+        while lines[ind] !="  Tests with different number of test files:":
+            miss_proc[lines[ind].strip()]=lines[ind+2].strip()
+            ind+=4
+    return miss_th,miss_proc
+def get_data(name):
+    data={}
+    with open('test_nums.csv','r') as csvfile:
+        fields=csvfile.readline().strip().split(",")
+        name_i=fields.index(name)
+        line=csvfile.readline()
+        while line !="":
+            entry=line.strip().split(",")
+            data[entry[0]]=float(entry[name_i])
+            line=csvfile.readline()
+    return data
