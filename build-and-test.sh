c8765c234cb9a8d48b17e0db92ee920086292dc5
Mohammed S. Jamil <msj2340@gmail.com>
2020-11-28 19:17:39 -0500

    Update build-and-test.sh

--- build-and-test.sh
+++ build-and-test.sh
@@ -35,6 +35,7 @@ time $WORKSPACE/cactusjenkins/build-cactus manifest/einsteintoolkit.th
 sed -i '2a export WORKSPACE=$PWD ' cactusjenkins/test-cactus
 sed -i '2a export JOB_NAME="TestJob01" ' cactusjenkins/test-cactus
 sed -i '2a set -x ' cactusjenkins/test-cactus
+sed -i '152a python3 store.py' cactusjenkins/test-cactus
 
 time $WORKSPACE/cactusjenkins/test-cactus all
 python3 $WORKSPACE/logpage.py
