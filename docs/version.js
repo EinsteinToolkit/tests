52fdc517a1d8fd99219d6a54910f43921ad82ecf
Mohammed Jamil <msj2340@gmail.com>
2021-04-03 11:38:15 -0400

    accidental delete

---
+++ docs/version.js
@@ -0,0 +1,14 @@
+var membersFile = new XMLHttpRequest();
+membersFile.open("GET", "version.txt", false);
+membersFile.send();
+lines = membersFile.responseText
+
+var version=parseInt(lines)-1
+
+text="";
+
+for(i=version;i>=1;i--){
+    text+='<a href="index_'+i+'.html">Build #'+i+'</a>\n'
+};
+sidebar=document.getElementsByClassName('sidebar');
+sidebar[0].innerHTML=text;
