3ae8a32248de4541a99c30f70d4ee8a746b1921a
Mohammed Jamil <msj2340@gmail.com>
2021-04-13 21:53:04 -0400

    moved badge

--- docs/version.js
+++ docs/version.js
@@ -5,10 +5,12 @@ lines = membersFile.responseText
 
 var version=parseInt(lines)-1
 
-text="";
+text='<img src="https://github.com/mojamil/einsteintoolkit/actions/workflows/main.yml/badge.svg" style="display:block;margin-left: auto;margin-right: auto;">';
 
 for(i=version;i>=1;i--){
-    text+='<a href="index_'+i+'.html">Build #'+i+'</a>\n'
+    
+    text+='<a href="index_'+i+'.html">Build #'+i+'</a>'
+
 };
 sidebar=document.getElementsByClassName('sidebar');
 sidebar[0].innerHTML=text;
