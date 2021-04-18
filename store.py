3705b9ab0e9bf1994740983e79f0223de74f60aa
Mohammed Jamil <msj2340@gmail.com>
2021-04-18 00:03:44 -0400

    fixed file issue

--- store.py
+++ store.py
@@ -3,16 +3,17 @@ import shutil,os,glob
 def copy_tests(test_dir,version):
     dst=f"./records/version_{version}/sim_{version}"
     dirlist=os.listdir(test_dir)
-    os.mkdir(dst)
+    if not os.path.isdir(dst):
+        os.mkdir(dst)
     for dir in dirlist:
-        if(os.path.isdir(test_dir+"/"+dir)):
+        if(not os.path.isdir(dst+"/"+dir)):
             os.mkdir(dst+"/"+dir)
-            diffs=[x.split("/")[-1] for x in glob.glob(test_dir+"/"+dir+"/*.diffs")]
-            logs=[x.split("/")[-1] for x in glob.glob(test_dir+"/"+dir+"/*.log")]
-            for log in logs:
-                shutil.copy(test_dir+"/"+dir+"/"+log,dst+"/"+dir+"/"+log)
-            for diff in diffs:
-                shutil.copy(test_dir+"/"+dir+"/"+diff,dst+"/"+dir+"/"+diff)
+        diffs=[x.split("/")[-1] for x in glob.glob(test_dir+"/"+dir+"/*.diffs")]
+        logs=[x.split("/")[-1] for x in glob.glob(test_dir+"/"+dir+"/*.log")]
+        for log in logs:
+            shutil.copy(test_dir+"/"+dir+"/"+log,dst+"/"+dir+"/"+log)
+        for diff in diffs:
+            shutil.copy(test_dir+"/"+dir+"/"+diff,dst+"/"+dir+"/"+diff)
 
     #shutil.copytree(test_dir,dst,)
 
@@ -45,8 +46,10 @@ def get_version():
     return current+1
 
 if __name__ == "__main__":
-    dir=os.path.expanduser("~/simulations/TestJob01_temp_1/output-0000/TEST/sim")
+    dir1=os.path.expanduser("~/simulations/TestJob01_temp_1/output-0000/TEST/sim")
+    dir2=os.path.expanduser("~/simulations/TestJob01_temp_2/output-0000/TEST/sim")
     version=get_version()
     os.mkdir(f"./records/version_{version}/")
     copy_builds(version)
-    copy_tests(dir,version)
+    copy_tests(dir1,version)
+    copy_tests(dir2,version)
