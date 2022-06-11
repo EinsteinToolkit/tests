3cc1a90ab46759ba4d648c6f0df27ac58e2c07ba
Roland Haas <rhaas@illinois.edu>
2022-06-10 23:22:52 -0500

    sidebar: remove hard-coded reference to mojamil

--- docs/version.js
+++ docs/version.js
@@ -3,8 +3,10 @@ membersFile.open("GET", "version.txt", false);
 membersFile.send();
 lines = membersFile.responseText.split("\n")
 
-
-text='<img src="https://github.com/mojamil/einsteintoolkit/actions/workflows/main.yml/badge.svg" style="display:block;margin-left: auto;margin-right: auto;">';
+// assuming it's a github.io page the first part of the FQDN is the username on
+// github
+user = window.location.host.split('.')[0]
+text='<img src="https://github.com/'+user+'/einsteintoolkit/actions/workflows/main.yml/badge.svg" style="display:block;margin-left: auto;margin-right: auto;">';
 
 lines.forEach(element => {
     text+='<a href="index_'+element+'.html">Build #'+element+'</a>'
