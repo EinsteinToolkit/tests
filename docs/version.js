da9baa8d13beb8e4445ab20732fd61d9009e6bb1
github runner <maintainers@einsteintoolkit.org>
2022-11-17 03:48:11 +0000

    Regenerated all HTML files

--- docs/version.js
+++ docs/version.js
@@ -1,51 +1,31 @@
 try {
-    var membersFile = new XMLHttpRequest();
-    membersFile.open("GET", "version.txt", false);
-    membersFile.send();
-    lines = membersFile.responseText.split("\n");
-} catch (err) {
-    console.log(err);
+    var workflowFile = new XMLHttpRequest();
+    workflowFile.open("GET", "https://api.github.com/repos/EinsteinToolkit/tests/actions/runs", false);
+    workflowFile.send();
+
+    var jsonResponse = JSON.parse(workflowFile.responseText);
+    count = jsonResponse["total_count"];
+    if (count > 0) {
+        latest_run = jsonResponse["workflow_runs"][0];
+        console.log(latest_run);
+        stat = latest_run.status; // completed, requested, in-progress
+        conclusion = latest_run.conclusion; // success, failure
+    }
+} catch (e) {
+  console.log(e.message);
 }
 
-// Assuming it's a github.io page the first part of the FQDN is the username on
-// GitHub and the first part of the path (after the initial /) is the repo name
-user = window.location.host.split('.')[0];
-repo_name = window.location.pathname.split('/')[1];
+if (conclusion == "failure") {
+    badge_to_display = "failing-status.svg"
+} else if (stat == "completed") {
+    badge_to_display = "passing-status.svg" 
+} else {
+    badge_to_display = "pending-status.svg" 
+}
 
-text ='<img src="https://github.com/einsteintoolkit/tests/actions/workflows/main.yml/badge.svg" style="display:block;margin-left: auto;margin-right: auto;">';
-
-lines.slice().sort((a, b) => a - b).reverse().forEach(element => {
-    if (element != '') { // skips empty element at end of file
-        try {
-            // Get the corresponding HTML file to check build status
-            var req = new XMLHttpRequest();
-            req.open("GET", "index_" + element + ".html", false);
-            req.send();
-            curr_html_contents = req.responseText;
-            // Create dummy DOM element to parse HTML element h1
-            var el = document.createElement('html');
-            el.innerHTML = curr_html_contents;
-            build_status = el.getElementsByTagName('h1')[0].innerHTML; 
-        } catch (err) {
-            console.log(err);
-        }
-
-        if (build_status == 'Some Tests Failed' || build_status == 'No Tests Available') {
-            // text += '<a href="index_'+ element +'.html" style="color:indianred;"> Build #'+ element +' </a>'
-            text += '<a href="index_'+ element +'.html"> Build #'+ element +' </a>'
-                   + '<img src="exclamation.svg" style="display: inline; width: 30px; height: 30px; float: right; margin-right: 14px;">';
-        } else {
-            // text += '<a href="index_'+ element + '.html" style="color:lawngreen;"> Build #' + element + ' </a>'
-            text += '<a href="index_'+ element + '.html"> Build #' + element + ' </a>'
-            + '<img src="check.svg" style="display: inline; width: 27px; height: 27px; float: right; margin-right: 15px;">';
-        }
-    }
-});
-    
-    
 try {
-    sidebar = document.getElementsByClassName('sidebar');
-    sidebar[0].innerHTML = text;
+    sidebar = document.getElementsByClassName('workflow-status');
+    sidebar[0].innerHTML = '<img src="' + badge_to_display + '" style="display:block; margin-left: auto; margin-right: auto;">';
 } catch (err) {
     console.log(err);
 }
