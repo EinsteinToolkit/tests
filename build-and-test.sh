6ad4c99b28ed36f10cbf6da30ad7b4b66b7277c7
Nadine Kuo <hnkuo2@illinois.edu>
2022-11-30 16:09:06 -0600

    Modify test-cactus, no sed anymore

--- build-and-test.sh
+++ build-and-test.sh
@@ -8,7 +8,6 @@ echo "Gh-pages dir to be passed to store.py: $2";
 export SYNC_SUBMODULES=true
 export CLEAN_CACTUS_JENKINS=true
 export BUILD_TYPE=Incremental
-# TODO: debug: in GitHub this is $GITHUB_WORKSPACE/scripts
 export WORKSPACE=$PWD
 export MASTER=$1
 export GH_PAGES=$2
@@ -22,7 +21,7 @@ export ENABLED_THORNS="
   ExternalLibraries/PETSc
 "
 
-# TODO: remove this? Or access via gh-pages
+# TODO: remove this? Or access via GH_PAGES?
 rm -f build__*.log
 # Work around bugs in Jenkins
 
@@ -47,15 +46,7 @@ fi
 # Need to force formattting of time so that we can parse it later
 export LC_TIME=C
 
-# TODO: make changes to local files, no sed
-# TODO: pass MASTER and GH_PAGES to the referenced scripts
+# TODO: pass MASTER and GH_PAGES to the referenced scripts (check final line in test-cactus)
 # This outputs three times: real, user and sys
 time $WORKSPACE/build-cactus $MASTER/manifest/einsteintoolkit.th 2>&1 | tee ./build.log
-sed -i '2a export WORKSPACE=$PWD ' test-cactus
-sed -i '2a export JOB_NAME="TestJob01" ' test-cactus
-sed -i '2a set -x ' test-cactus
-sed -i '/rm -rf \$simdir\/\$simname/d' test-cactus
-sed -i '43a rm -rf \$simdir\/\$simname' test-cactus
-# TODO: check if gh-pages is properly passed
-sed -i -e '$a python3 store.py . $GH_PAGES $HOME/simulations/TestJob01_temp_1/output-0000/TEST/sim $HOME/simulations/TestJob01_temp_2/output-0000/TEST/sim || true' test-cactus
 time $WORKSPACE/test-cactus all
