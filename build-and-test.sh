c4391dd031131c83d8780340438c3725625e1716
Nadine Kuo <hnkuo2@illinois.edu>
2022-12-09 21:26:23 -0600

    Remove positional args in bash scripts

--- build-and-test.sh
+++ build-and-test.sh
@@ -70,5 +70,5 @@ chmod +x cactus/build-cactus
 # "2>" redirects stderr to an (unspecified) file, "&1" redirects stderr to stdout.
 # "tee" reads from the standard input and writes to (new) ./build.log file
 # store.py copies this ./build.log file to {gh_pages}/records/version_{version}/build_{version}.log
-time cactus/build-cactus $MASTER/manifest/einsteintoolkit.th 2>&1 | tee ./build.log
-time cactus/test-cactus all
+time cactus/build-cactus --thornlist $MASTER/manifest/einsteintoolkit.th 2>&1 | tee ./build.log
+time cactus/test-cactus --testmode all
