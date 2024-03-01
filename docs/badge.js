9da5a63ca002009fcb771df18dc8d522d024be89
github runner <maintainers@einsteintoolkit.org>
2024-03-01 20:14:13 +0000

    CI workflow updated html file

---
+++ docs/badge.js
@@ -0,0 +1,35 @@
+try {
+    var workflowFile = new XMLHttpRequest();
+    workflowFile.open("GET", "https://api.github.com/repos/EinsteinToolkit/tests/actions/workflows/main.yml/runs", false);
+    workflowFile.send();
+
+    var jsonResponse = JSON.parse(workflowFile.responseText);
+    count = jsonResponse["total_count"];
+    if (count > 0) {
+        latestRun = jsonResponse["workflow_runs"][0];
+        stat = latestRun.status; // completed, requested, in-progress
+        conclusion = latestRun.conclusion; // success, failure, cancelled, null
+        actionLink = latestRun.html_url; // link to (pending) workflow in GitHub
+    }
+} catch (e) {
+  console.log(e.message);
+}
+
+// workflowLink = "https://einsteintoolkit.github.io/tests/";
+workflowLink= actionLink;
+if (conclusion == "failure") {
+    badgeToDisplay = "failing-status.svg";
+} else if (stat == "completed") {
+    badgeToDisplay = "passing-status.svg";
+} else {
+    badgeToDisplay = "pending-status.svg";
+}
+
+try {
+    badgeLink = document.getElementsByClassName('workflow-status');
+    badgeLink[0].innerHTML = '<a href="' + workflowLink + '" style="display:block; margin-left: auto; margin-right: auto;">' 
+                         + '<img src="' + badgeToDisplay + '">'
+                         + '</a>';
+} catch (err) {
+    console.log(err);
+}
