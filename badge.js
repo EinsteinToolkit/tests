827f771dcb5526c762dc8a8f91fdaecd3c4781f3
Roland Haas <rhaas@illinois.edu>
2024-03-06 14:43:10 -0600

    handle pending runs in badge

--- badge.js
+++ badge.js
@@ -1,3 +1,4 @@
+var stat, conclusion, actionlink;
 try {
     var workflowFile = new XMLHttpRequest();
     workflowFile.open("GET", "https://api.github.com/repos/EinsteinToolkit/tests/actions/workflows/main.yml/runs", false);
@@ -6,7 +7,18 @@ try {
     var jsonResponse = JSON.parse(workflowFile.responseText);
     count = jsonResponse["total_count"];
     if (count > 0) {
-        latestRun = jsonResponse["workflow_runs"][0];
+        var run_number = 0;
+        for(var i = 0 ; i < jsonResponse["workflow_runs"].length ; i++) {
+            latestRun = jsonResponse["workflow_runs"][i];
+            stat = latestRun.status; // completed, requested, in_progress
+            console.log(i, " stat ", stat);
+            if (stat == "completed" || stat == "in_progress") {
+                run_number = i;
+                console.log("run number: "+i);
+                break;
+            }
+        }
+        latestRun = jsonResponse["workflow_runs"][run_number];
         stat = latestRun.status; // completed, requested, in-progress
         conclusion = latestRun.conclusion; // success, failure, cancelled, null
         actionLink = latestRun.html_url; // link to (pending) workflow in GitHub
@@ -16,13 +28,19 @@ try {
 }
 
 // workflowLink = "https://einsteintoolkit.github.io/tests/";
-workflowLink= actionLink;
-if (conclusion == "failure") {
-    badgeToDisplay = "failing-status.svg";
-} else if (stat == "completed") {
-    badgeToDisplay = "passing-status.svg";
-} else {
+workflowLink = actionLink;
+if (stat == "completed") {
+    if (conclusion == "failure") {
+        badgeToDisplay = "failing-status.svg";
+    } else if (conclusion == "success") {
+        badgeToDisplay = "passing-status.svg";
+    } else {
+        badgeToDisplay = "unknown";
+    }
+} else if (stat == "in_progress") {
     badgeToDisplay = "pending-status.svg";
+} else {
+    badgeToDisplay = "unknown";
 }
 
 try {
